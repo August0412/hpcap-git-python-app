345a7dde1589e3eb33dc7dc030e55733aa1f15d9
atharv <atharvganerunstopable@gmail.com>
2024-11-15 15:16:23 +0530

    Added a for loop to print some technologies.

--- calc.py
+++ calc.py
@@ -6,7 +6,16 @@ app = Flask(__name__)
 def home():
     name = "Atharv Ganer" 
     pnr = "240840141004" 
+    # List of technologies
+    technologies = ['Python', 'Flask', 'JavaScript', 'HTML', 'CSS', 'SQL', 'Docker']
+    
+    # Print technologies using a for loop in Python
+    print("Technologies:")
+    for tech in technologies:
+        print(tech)
     return render_template('index.html', name=name, pnr=pnr)
+
+print "This is used for git"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)  
